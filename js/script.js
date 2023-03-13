34c3feede024c1fb6fdfe3ef9fb4c0448fa16e44
aikow <92host26@gmail.com>
2023-03-13 15:33:55 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+//alert("hello");
+
+//const result = confirm('Are you here?');
+//console.log(result);
+
+//const answer = +prompt('Vam est 18?','');
+//console.log(answer+5);
+//console.log(typeof(answer));
+
+//const answers = [];
+//answers[0] = prompt('Ваше имя?', '');
+//answers[1] = prompt('Ваш возраст', '');
+//answers[2] = prompt('Ваш город?', '');
+
+//console.log(answers);
+//document.write(answers);
+
+//const result = confirm("Are you here?");
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Ivan';
+
+alert(`Hello, ${user}`);
+
+
