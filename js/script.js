6d5d631726632d9efc5df49902bfa96f938ce7eb
aikow <92host26@gmail.com>
2023-03-15 14:19:08 +0600

    pervoe zadaniye

--- js/script.js
+++ js/script.js
@@ -19,12 +19,46 @@
 
 //const result = confirm("Are you here?");
 
-const category = 'toys';
+//const category = 'toys';
 
-console.log(`https://someurl.com/${category}/5`);
+/*console.log(`https://someurl.com/${category}/5`);
 
 const user = 'Ivan';
 
 alert(`Hello, ${user}`);
 
 
+
+const user = 'Ivan';
+
+const user2 = 'Igor';
+
+console.log(user + user2);
+
+console.log("ivan" + "Igor");*/
+
+//1-hw
+
+//--------------
+
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?"," ");
+
+const personalMovieDB = {
+        
+    count: numberOfFilms,
+    movies: {},     
+    actors: {} ,
+    genres: [],
+    privat: false
+
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('Насколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('Насколько оцените его?', '');
+
+personalMovieDB.movies[a]=b;
+personalMovieDB.movies[c]=d;
+
+console.log(personalMovieDB);
